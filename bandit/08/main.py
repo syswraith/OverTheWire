358fa95dfa34f302d6032c456446478c9d09c9e4
syswraith <syswraith@proton.me>
2025-08-28 13:52:02 +0530

    07, 08, 09

---
+++ bandit/08/main.py
@@ -0,0 +1,38 @@
+import os
+from pwn import *
+import enchant
+
+URL = 'bandit.labs.overthewire.org'
+PORT = 2220
+
+USER = 'bandit7'
+MAGIC_COMMAND = 'grep -r "millionth" ~'
+
+with open('password.txt', 'r') as file:
+    PASSWORD = file.read()
+
+eng_dictionary = enchant.Dict('en_US')
+shell = ssh(USER, URL, password=PASSWORD, port=PORT)
+
+#interactive_shell = shell.process('/bin/sh')
+#interactive_shell.interactive()
+
+sh = shell.run(MAGIC_COMMAND)
+output = sh.recvallS()
+
+wordlist = output.split()
+possible_passwords = [string for string in wordlist if not eng_dictionary.check(string)]
+
+for passw in possible_passwords: 
+    if len(passw) == 32:
+        with open('password.txt', 'w') as file: file.write(passw)
+
+print()
+print('Possible passwords:')
+for x in possible_passwords: print(f'- {x}')
+print()
+print('Original output:')
+print(output)
+print()
+
+shell.close()
